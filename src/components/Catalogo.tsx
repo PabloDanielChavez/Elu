f71f7a9b1a0a20a220d1c5218cdfc8eb8fd69bc0
Pablo Daniel Chavez <pablo_daniel_chavez@outlook.es>
2026-04-29 14:38:55 -0300

    modificaciones en img y en el componente de catalogo

--- src/components/Catalogo.tsx
+++ src/components/Catalogo.tsx
@@ -5,90 +5,46 @@ import Image from "next/image";
 import Link from "next/link";
 import { IoLogoWhatsapp } from "react-icons/io";
 
+const productos = [
+    { id: 1, nombre: "Borcego", desc: "Cuero, suela de caucho y máximo confort para el trabajo.", precio: "70.000", img: "/img/1.png", talle: `39 - 46` },
+    { id: 2, nombre: "Zapato", desc: "Diseño minimalista en cuero, con acabados artesanales.", precio: "70.000", img: "/img/2.png" , talle: `39 - 46` },
+    { id: 3, nombre: "Zapatillas", desc: "descripcion", precio: "50.000", img: "/img/3.png" , talle: `39 - 46` },
+    { id: 3, nombre: "Zapato de Mujer", desc: "descripcion", precio: "50.000", img: "/img/4.png" , talle: `39 - 46` },
+    { id: 3, nombre: "Sandalias", desc: "descripcion", precio: "50.000", img: "/img/5.png" , talle: `39 - 46` },
+];
+
 export default function Catalogo() {
     return (
         <section className={style_catalogo.catalogo}>
-            <div className={style_catalogo.catalogo_grid}>
-                <div className={style_catalogo.catalogo_card}>
-                    <div className={style_catalogo.catalogo_imagen_container}>
-                        <Image 
-                            className={style_catalogo.catalogo_img}
-                            src="/img/1.png" 
-                            alt="Borcegos"
-                            width={200}
-                            height={200}
-                            priority
-                        />
-                        <span className={style_catalogo.catalogo_rating}>4.9 ★</span>
-                    </div>
-                    <div className={style_catalogo.catalogo_info}>
-                        <h3 className={style_catalogo.catalogo_titulo}>Borcegos</h3>
-                        <p className={style_catalogo.catalogo_desc}>Cuero sintético, suela de caucho y máximo confort para el día a día.</p>
-                        <div className={style_catalogo.catalogo_footer}>
-                            <span className={style_catalogo.catalogo_precio}>$25.000</span>
-                            <Link
-                                href={`https://wa.me/5491158007420?text=https://elu-r0f5.onrender.com%20Hola!%20Me%20gustaría%20más%20información%20sobre:%Borcegos`}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className={style_catalogo.catalogo_btn_add}
-                            ><IoLogoWhatsapp /></Link>
+            <div className={style_catalogo.catalogo_grid}>   
+                {productos.map((prod) => (
+                    <div className={style_catalogo.catalogo_card}>
+                        <div className={style_catalogo.catalogo_imagen_container}>
+                            <Image 
+                                className={style_catalogo.catalogo_img}
+                                src={prod.img}
+                                alt={prod.nombre}
+                                width={200}
+                                height={200}
+                                priority
+                            />
+                            <span className={style_catalogo.catalogo_rating}>{prod.talle}</span>
+                        </div>
+                        <div className={style_catalogo.catalogo_info}>
+                            <h3 className={style_catalogo.catalogo_titulo}>{prod.nombre}</h3>
+                            <p className={style_catalogo.catalogo_desc}>{prod.desc}</p>
+                            <div className={style_catalogo.catalogo_footer}>
+                                <span className={style_catalogo.catalogo_precio}>${prod.precio}</span>
+                                <Link
+                                    href={`https://wa.me/5491158007420?text=https%3A%2F%2Felucalzados.netlify.app%0AHola!%20Me%20gustaría%20más%20información%20sobre:%0AProducto:%20${prod.nombre}%0APrecio:%20$${prod.precio}%0ADescripción:%20${prod.desc}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className={style_catalogo.catalogo_btn_add}
+                                ><IoLogoWhatsapp /></Link>
+                            </div>
                         </div>
                     </div>
-                </div>
-                
-                <div className={style_catalogo.catalogo_card}>
-                    <div className={style_catalogo.catalogo_imagen_container}>
-                        <Image 
-                            className={style_catalogo.catalogo_img}
-                            src="/img/2.png" 
-                            alt="Zapato Deportivo"
-                            width={200}
-                            height={200}
-                            priority
-                        />
-                        <span className={style_catalogo.catalogo_rating}>5.0 ★</span>
-                    </div>
-                    <div className={style_catalogo.catalogo_info}>
-                        <h3 className={style_catalogo.catalogo_titulo}>Zapato</h3>
-                        <p className={style_catalogo.catalogo_desc}>Diseño minimalista en cuero legítimo con acabados artesanales.</p>
-                        <div className={style_catalogo.catalogo_footer}>
-                            <span className={style_catalogo.catalogo_precio}>$28.2000</span>
-                            <Link
-                                href={`https://wa.me/5491158007420?text=https://elu-r0f5.onrender.com%20Hola!%20Me%20gustaría%20más%20información%20sobre:%Zapato`}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className={style_catalogo.catalogo_btn_add}
-                            ><IoLogoWhatsapp /></Link>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={style_catalogo.catalogo_card}>
-                    <div className={style_catalogo.catalogo_imagen_container}>
-                        <Image 
-                            className={style_catalogo.catalogo_img}
-                            src="/img/3.png" 
-                            alt="Zapatillas"
-                            width={200}
-                            height={200}
-                            priority
-                        />
-                        <span className={style_catalogo.catalogo_rating}>4.7 ★</span>
-                    </div>
-                    <div className={style_catalogo.catalogo_info}>
-                        <h3 className={style_catalogo.catalogo_titulo}>Zapatillas</h3>
-                        <p className={style_catalogo.catalogo_desc}>Resistencia y distinción. Ideal para marcas de alto rendimiento.</p>
-                        <div className={style_catalogo.catalogo_footer}>
-                            <span className={style_catalogo.catalogo_precio}>$32.000</span>
-                            <Link
-                                href={`https://wa.me/5491158007420?text=https://elu-r0f5.onrender.com%20Hola!%20Me%20gustaría%20más%20información%20sobre:%Zapatillas`}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className={style_catalogo.catalogo_btn_add}
-                            ><IoLogoWhatsapp /></Link>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
         </section>
     );
